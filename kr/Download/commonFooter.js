8bb28f0b91510e86aaa67119404a47d1fd03d35a
blackplus2025 <taikoplus2025@gmail.com>
2025-07-24 23:59:16 +0900

    20250725

---
+++ kr/Download/commonFooter.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+document.write("\
+  <link rel=\"stylesheet\" href=\"//bandainamco-am.co.jp/am/css/commonFooter.css\" media=\"all\" />\
+  <footer id=\"commonPageFooter\">\
+	<div class=\"wrapper\">\
+		<div class=\"logo\"><img src=\"//bandainamco-am.co.jp/shared/images/logo_am_sp.png\" alt=\"\" /></div>\
+		<p class=\"commonCopyright\"><small>&copy;Bandai Namco Amusement Inc.</small></p>\
+	</div>\
+</footer>\
+  ");
+
+  var now = new Date();
+document.write(unescape("%3Cscript src='" + (("https:" == document.location.protocol) ? "https://" : "http://") + "bandainamco-am.co.jp/am/js/tracklist.js?" + now.getTime() + "'type='text/javascript'%3E%3C/script%3E"));
